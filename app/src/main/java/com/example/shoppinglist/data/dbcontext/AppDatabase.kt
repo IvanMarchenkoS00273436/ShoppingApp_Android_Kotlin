583cab86407a910ffd9f03dadae6caea86cb82a5
Ivan Marchenko <vania9023@gmail.com>
2025-11-27 18:52:15 +0000

    add Converter

--- app/src/main/java/com/example/shoppinglist/data/dbcontext/AppDatabase.kt
+++ app/src/main/java/com/example/shoppinglist/data/dbcontext/AppDatabase.kt
@@ -2,15 +2,17 @@ package com.example.shoppinglist.data.dbcontext
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.shoppinglist.data.daos.CategoryDao
 import com.example.shoppinglist.data.daos.ProductDao
 import com.example.shoppinglist.data.daos.UserDao
 import com.example.shoppinglist.data.entities.Category
 import com.example.shoppinglist.data.entities.Product
 import com.example.shoppinglist.data.entities.User
-
+import com.example.shoppinglist.data.convertors.Converters
 
 @Database(entities = [User::class, Product::class, Category::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun productDao(): ProductDao
