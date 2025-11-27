8eafb016dbe52663f7d2741f874faac61e056017
Ivan1 <S00273436@atu.ie>
2025-11-27 12:00:18 +0000

    add ProductDao

---
+++ app/src/main/java/com/example/shoppinglist/daos/ProductDao.kt
@@ -0,0 +1,36 @@
+package com.example.shoppinglist.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.shoppinglist.entities.Product
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductDao {
+    // --- Create ---
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(product: Product): Long
+
+    // --- Read ---
+    @Query("SELECT * FROM products ORDER BY DateAdded DESC")
+    fun getAllProducts(): Flow<List<Product>>
+
+    @Query("SELECT * FROM products WHERE Id = :productId")
+    suspend fun getProductById(productId: Long): Product?
+
+    @Query("SELECT * FROM products WHERE product_category_id = :categoryId")
+    fun getProductsByCategory(categoryId: Long): Flow<List<Product>>
+
+    // --- Update ---
+    @Update
+    suspend fun update(product: Product)
+
+    // --- Delete ---
+    @Delete
+    suspend fun delete(product: Product)
+
+}
