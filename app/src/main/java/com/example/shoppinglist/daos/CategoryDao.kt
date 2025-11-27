c72ad2112ae2455700d5af89248026660f032a7b
Ivan1 <S00273436@atu.ie>
2025-11-27 11:50:10 +0000

    Add CategoryDao

---
+++ app/src/main/java/com/example/shoppinglist/daos/CategoryDao.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglist.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.shoppinglist.entities.Category
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+    // --- Create ---
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(category: Category): Long
+
+    // --- Read ---
+    @Query("SELECT * FROM categories ORDER BY category_name ASC")
+    fun getAllCategories(): Flow<List<Category>>
+
+    @Query("SELECT * FROM categories WHERE category_id = :id")
+    suspend fun getCategoryById(id: Long): Category?
+
+    // --- Update ---
+    @Update
+    suspend fun update(category: Category)
+
+    // --- Delete ---
+    @Delete
+    suspend fun delete(category: Category)
+}
